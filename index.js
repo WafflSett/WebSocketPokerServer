e7bbcfbef802c2529c376db4e152f4fe51989795
bandi3028 <andris.moldovan@gmail.com>
2025-05-19 09:41:30 +0200

    userlist -- init

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ server.on('connection', (socket) => {
                 tableId: myTableId,
                 userList: userList
             }))
-            broadcastToTable(currTable, { type: 'join', userId: clientId, userName: msg.userName, tableId: myTableId, position: position });
+            broadcastToTable(currTable, { type: 'join', userId: clientId, userName: msg.userName, tableId: myTableId, position: position, userList: userList});
             console.log(`init: U${clientId} conn. to T${tableId} @pos ${position}`);
             return;
         }
